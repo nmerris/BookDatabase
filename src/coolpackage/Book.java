7e90bb72e7a58dbd9d1654ff3d67e61aa44fa537
nmerris <nate.merris@gmail.com>
2017-07-28 14:22:12 -0400

    generate constructor, getters, setters in Book

--- src/coolpackage/Book.java
+++ src/coolpackage/Book.java
@@ -1,12 +1,109 @@
-/**
- * 
- */
 package coolpackage;
 
 /**
- * @author student
+ * @author Nathan Merris
  *
  */
 public class Book {
 
+
+
+	private String sku;
+	private String title;
+	private String author;
+	private String description;
+	private double price;
+	
+	
+	public Book(String sku, String title, String author, String description, double price) {
+		this.sku = sku;
+		this.title = title;
+		this.author = author;
+		this.description = description;
+		this.price = price;
+	}
+
+	
+
+	/**
+	 * @return the sku
+	 */
+	public String getSku() {
+		return sku;
+	}
+
+
+	/**
+	 * @param sku the sku to set
+	 */
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+
+
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
 }
--- src/coolpackage/BookDatabaseApp.java
+++ src/coolpackage/BookDatabaseApp.java
@@ -3,7 +3,13 @@ package coolpackage;
 public class BookDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		
 
 	}
 
