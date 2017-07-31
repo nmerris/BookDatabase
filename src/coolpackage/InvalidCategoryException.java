59c2751c5e6993eeaf5c1a26c57bcd70c5d49f7a
nmerris <nate.merris@gmail.com>
2017-07-31 11:37:50 -0400

    update InvalidCategoryException javadoc

--- src/coolpackage/InvalidCategoryException.java
+++ src/coolpackage/InvalidCategoryException.java
@@ -1,15 +1,15 @@
 package coolpackage;
 
-
+/**
+ * Throw this exception if the user enters an invalid category from Book.
+ * Use it's toString method to output an appropriate message.
+ * 
+ * @author Nathan Merris
+ * @see coolpackage.Book#categories
+ *
+ */
 public class InvalidCategoryException extends Exception {
-	/**
-	 * Throw this exception if the user enters an invalid category from Book.
-	 * Print this using toString to output an appropriate message.
-	 * 
-	 * @author Nathan Merris
-	 *
-	 */
-	
+
 	@Override
 	public String toString() {
 		return "Please enter a category exactly as given";
